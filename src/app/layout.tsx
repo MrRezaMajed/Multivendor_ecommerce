272d7d02d94b2bdee01aa84d5071b7de655535e8
MrRezaMajed <153994580+MrRezaMajed@users.noreply.github.com>
2025-05-04 04:46:30 -0700

    ThemeToggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Barlow } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,7 +34,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${barlowFont.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider attribute="class" defaultTheme="light"
+        enableSystem
+        disableTransitionOnChange>
+         {children}
+        </ThemeProvider>
       </body>
     </html>
   );
